5805025c0e34790330737ef94dae5287987b4d35
Identitynull <identitynulll@gmail.com>
2025-10-04 08:53:00 +0500

    a

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import "./Login.css";
 
 function Login() {
   return (
-    <div>
-
-<spline-viewer url="https://prod.spline.design/9bPsy1MuDNUJZS2T/scene.splinecode"></spline-viewer>
+    <div className="body">
+      <script
+        type="module"
+        src="https://unpkg.com/@splinetool/viewer@1.10.74/build/spline-viewer.js"
+      ></script>
+      <spline-viewer url="https://prod.spline.design/a6NfPnCxiTXRnhWw/scene.splinecode" height="1050"></spline-viewer>
     </div>
   );
 }
