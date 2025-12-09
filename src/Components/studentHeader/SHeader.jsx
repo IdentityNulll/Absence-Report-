12ccc6a21e8fe625c24ab8d0eeccb631e1d862b3
Identitynull <identitynulll@gmail.com>
2025-12-09 17:59:53 +0500

    annoying errors

--- src/Components/studentHeader/SHeader.jsx
+++ src/Components/studentHeader/SHeader.jsx
@@ -4,8 +4,45 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
 import Sidebar from "../studentSidebar/SSidebar";
 import "./SHeader.css";
+import api from "../../api/axios";
+import { useState, useEffect } from "react";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header({ adminData }) {
+  const [student, setStudent] = useState(null);
+  const [profileImage, setProfileImage] = useState(null);
+
+  useEffect(() => {
+    const fetchStudentImage = async () => {
+      const id = localStorage.getItem("id");
+      try {
+        const res = await api.get(`/student/${id}`);
+        const data = res.data?.data;
+
+        if (data) {
+          setStudent(data);
+
+          if (data.photoUrl) {
+            // Fetch the image as blob
+            const imageRes = await api.get(data.photoUrl, {
+              responseType: "blob",
+            });
+            const imageBlob = imageRes.data;
+            const imageObjectUrl = URL.createObjectURL(imageBlob);
+            setProfileImage(imageObjectUrl);
+
+            // Save blob URL in memory, NOT in localStorage
+            // If you want caching, you must cache the API route, not the blob
+          }
+        }
+      } catch (err) {
+        console.error("Failed to fetch student image:", err);
+      }
+    };
+
+    fetchStudentImage();
+  }, []);
+
   return (
     <header className="admin-header">
       <div className="header-left">
@@ -19,7 +56,20 @@ export default function Header({ adminData }) {
         </Link>
 
         <Link to="/student/profile" className="profile-placeholder">
-          {adminData ? adminData.firstName?.[0]?.toUpperCase() : "S"}
+          {profileImage ? (
+            <img
+              src={profileImage}
+              alt="Profile"
+              className="avatar-img-big"
+              style={{ borderRadius: "50%" }}
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon={faUser}
+              className="default-avatar-big"
+              style={{ fontSize: "16px" }}
+            />
+          )}
         </Link>
       </div>
     </header>
